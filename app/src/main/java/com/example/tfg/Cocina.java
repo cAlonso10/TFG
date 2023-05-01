7510b138e849758e8a1cc18bf7e901a4f5a7a53c
cAlonso10 <carlosalonsomunoz@gmail.com>
2023-05-01 22:24:53 +0200

    Esqueleto cocina

--- app/src/main/java/com/example/tfg/Cocina.java
+++ app/src/main/java/com/example/tfg/Cocina.java
@@ -1,30 +1,183 @@
 package com.example.tfg;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Cocina extends AppCompatActivity {
 
-    Button botonOutC;
+
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    String emailUsuario;
+    ListView listViewPedidos;
+    List<String> listaPedidos = new ArrayList<>();
+
+    List<String> listaIdPedidos = new ArrayList<>();
+    ArrayAdapter<Map<String, Object>> mAdapterPedidos;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cocina);
 
-        getSupportActionBar().hide();
 
+
+
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(createCustomTitle());
+
+        db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
-        botonOutC = findViewById(R.id.outC);
-        botonOutC.setOnClickListener(view -> {
-           logout();
+        emailUsuario = mAuth.getCurrentUser().getEmail();
+        listViewPedidos = findViewById(R.id.ListView);
+
+        actualizarUI();
+
+        listViewPedidos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtener la información detallada del pedido seleccionado
+                Map<String, Object> pedido = mAdapterPedidos.getItem(position);
+                String producto = pedido.get("Producto").toString();
+                String cantidad = pedido.get("Cantidad").toString();
+                String estado = pedido.get("Estado").toString();
+                // Crear un diálogo emergente que muestre la información detallada del pedido
+                AlertDialog.Builder builder = new AlertDialog.Builder(Cocina.this);
+                builder.setTitle("Información del pedido " + (position + 1));
+                builder.setMessage("Producto: " + producto + " x" + cantidad + "\nEstado: " + estado);
+                builder.setPositiveButton("OK", null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
         });
+
+    }
+
+    private void actualizarUI() {
+        db.collection("Pedidos")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            return;
+                        }
+
+                        listaPedidos.clear();
+                        listaIdPedidos.clear();
+                        List<Map<String, Object>> listaPedidosMap = new ArrayList<>();
+
+
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            listaIdPedidos.add(doc.getId());
+                            listaPedidos.add(doc.getString("Estado"));
+                            Map<String, Object> pedido = new HashMap<>();
+                            pedido.put("Producto", doc.getString("Producto"));
+                            pedido.put("Cantidad", doc.getLong("Cantidad"));
+                            pedido.put("Estado", doc.getString("Estado"));
+                            listaPedidosMap.add(pedido);
+                        }
+                        if (listaPedidosMap.size() == 0) {
+                            listViewPedidos.setAdapter(null);
+                        } else {
+                            mAdapterPedidos = new ArrayAdapter<Map<String, Object>>(Cocina.this, R.layout.item_pedido, listaPedidosMap) {
+                                @NonNull
+                                @Override
+                                public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                                    if (convertView == null) {
+                                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_pedido, parent, false);
+                                    }
+                                    TextView PedidoTextView = convertView.findViewById(R.id.PedidoTextView);
+
+                                    TextView estadoTextView = convertView.findViewById(R.id.EstadoTextView);
+
+                                    Map<String, Object> pedido = getItem(position);
+                                    PedidoTextView.setText("Pedido" + (position + 1));
+                                    estadoTextView.setText(pedido.get("Estado").toString());
+
+                                    return convertView;
+                                }
+                            };
+                            listViewPedidos.setAdapter(mAdapterPedidos);
+                        }
+
+                    }
+                });
+
+    }
+
+
+    private TextView createCustomTitle() {
+        TextView textView = new TextView(this);
+        textView.setText("Pedidos");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.BLACK);
+        textView.setPadding(0, 0, 0, 0);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        return textView;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logout();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+
+
+        }
 
     }
     private void logout() {
@@ -38,7 +191,30 @@ public class Cocina extends AppCompatActivity {
         finish();
     }
 
+    public void cooked(View view) {
+        View parent = (View) view.getParent();
+        TextView estasdoTextView = parent.findViewById(R.id.EstadoTextView);
+        String pedido = estasdoTextView.getText().toString();
+        int posicion = listaPedidos.indexOf(pedido);
+        if (posicion >= 0) {
+            db.collection("Pedidos").document(listaIdPedidos.get(posicion)).delete();
+        }
 
     }
+
+    public void done(View view) {
+        View parent = (View) view.getParent();
+        TextView estasdoTextView = parent.findViewById(R.id.EstadoTextView);
+        String pedido = estasdoTextView.getText().toString();
+        int posicion = listaPedidos.indexOf(pedido);
+        if (posicion >= 0) {
+            db.collection("Pedidos").document(listaIdPedidos.get(posicion)).update("Estado", "Completado");
+        }
+
+    }
+
+
+
+}
 
 
--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -8,11 +8,19 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button botonOut;
+    Button botonOut, test;
     private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +33,48 @@ public class MainActivity extends AppCompatActivity {
             logout();
         });
 
+        test = findViewById(R.id.test);
+        test.setOnClickListener(view -> {
+            test();
+        });
+
     }
 
+    protected void test() {
+        // Crear un nuevo pedido con estado "En espera"
+        Map<String, Object> pedido = new HashMap<>();
+        pedido.put("estado", "En espera");
+
+        // Agregar los productos al pedido
+        List<Map<String, Object>> productos = new ArrayList<>();
+
+        Map<String, Object> producto1 = new HashMap<>();
+        producto1.put("nombre", "Pollo");
+        producto1.put("cantidad", 1);
+        productos.add(producto1);
+
+        Map<String, Object> producto2 = new HashMap<>();
+        producto2.put("nombre", "Arroz");
+        producto2.put("cantidad", 2);
+        productos.add(producto2);
+
+        Map<String, Object> producto3 = new HashMap<>();
+        producto3.put("nombre", "Cerveza");
+        producto3.put("cantidad", 3);
+        productos.add(producto3);
+
+        pedido.put("productos", productos);
+
+        // Agregar el pedido a la colección "pedidos" con un ID aleatorio generado por Firestore
+        db.collection("pedidos")
+                .add(pedido)
+                .addOnSuccessListener(documentReference -> {
+
+                })
+                .addOnFailureListener(e -> {
+
+                });
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -46,5 +94,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+    protected void test2() {
+        Map<String, Object> pedido = new HashMap<>();
+        pedido.put("estado", "En espera");
 
+        // Crear la colección "pedidos" y agregar el documento "pedido001" con el objeto Pedido
+        db.collection("pedidos").document("pedido001").set(pedido);
+
+        // Crear un objeto Producto con los campos nombre y cantidad
+        Map<String, Object> producto1 = new HashMap<>();
+        producto1.put("nombre", "Pollo");
+        producto1.put("cantidad", 1);
+
+        Map<String, Object> producto2 = new HashMap<>();
+        producto2.put("nombre", "Arroz");
+        producto2.put("cantidad", 2);
+
+        Map<String, Object> producto3 = new HashMap<>();
+        producto3.put("nombre", "Papas");
+        producto3.put("cantidad", 3);
+
+        // Agregar los documentos de productos a la subcolección "productos" del documento "pedido001"
+        db.collection("pedidos").document("pedido001").collection("productos").document("producto001").set(producto1);
+        db.collection("pedidos").document("pedido001").collection("productos").document("producto002").set(producto2);
+        db.collection("pedidos").document("pedido001").collection("productos").document("producto003").set(producto3);
+        logout();
+    }
 }
